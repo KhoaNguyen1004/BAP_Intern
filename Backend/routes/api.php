d0d6b97fc1e9e1224243e892708a98e04c5fe1da
KhoaNguyen1004 <nguyentrongkhoa104@gmail.com>
2024-07-03 10:42:36 +0700

    Update back-end code

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -8,17 +8,21 @@ Route::get('/not-authorized', function () {
     return response()->json(['error' => 'Unauthorized. Please login.'], 401);
 })->name('login');
 Route::post('/LoginProcessing', [UserController::class, 'LoginProcessing']);
-
+Route::get('/', [UserController::class, 'Show']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/', [UserController::class, 'Show']);
     Route::get('/GetAllTemplate', [UserController::class, 'GetAllTemplate']);
     Route::get('/GetTemplate/{template}', [UserController::class, 'GetTemplate']);
+    Route::post('/logout', [UserController::class, 'logout'])->name('logout');
     
     Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
         Route::post('/AddTemplate', [UserController::class, 'AddTemplate']);
+        Route::post('/CloneTemplate/{template}', [UserController::class, 'CloneTemplate']);
+        Route::post('/AddSecion', [UserController::class, 'AddSecion']);
         Route::put('/EditTemplate/{template}', [UserController::class, 'EditTemplate']);
         Route::delete('/DeleteTemplate/{template}', [UserController::class, 'DeleteTemplate']);
+        Route::delete('/DeleteSecion/{section}', [UserController::class, 'DeleteSecion']);
         Route::put('/{template}', [UserController::class, 'ChangeTemplate']);
+        Route::put('/EditSection/{section}', [UserController::class, 'EditSection']);
     });
 });
