a76dfe808635d4178e941a03a00a2941cd24ed7e
KhoaNguyen1004 <nguyentrongkhoa104@gmail.com>
2024-07-04 16:33:29 +0700

    Update backend code

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/not-authorized', function () {
-    return response()->json(['error' => 'Unauthorized. Please login.'], 401);
+    return response()->json([
+        'status' => 'fail',
+        'message' => 'Unauthorized, Please Login.'
+    ], 401);
 })->name('login');
 Route::post('/LoginProcessing', [UserController::class, 'LoginProcessing']);
 Route::get('/', [UserController::class, 'Show']);
@@ -14,7 +16,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/GetAllTemplate', [UserController::class, 'GetAllTemplate']);
     Route::get('/GetTemplate/{template}', [UserController::class, 'GetTemplate']);
     Route::post('/logout', [UserController::class, 'logout'])->name('logout');
-
+    
     Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
         Route::post('/AddTemplate', [UserController::class, 'AddTemplate']);
         Route::post('/CloneTemplate/{template}', [UserController::class, 'CloneTemplate']);
