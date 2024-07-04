fbcc5a6cfdb6a53378a4250e398730701b867cb3
kzi611 <dovy611@gmail.com>
2024-07-05 01:26:51 +0700

    fe - update section configpage

--- Frontend/src/features/configPage/configPage.jsx
+++ Frontend/src/features/configPage/configPage.jsx
@@ -114,7 +114,7 @@ const ConfigPage = () => {
               : () => handleEditFooter(footerContent)
         }
         onCancel={handleCancel}
-        okText="Save"
+        okText="Delete"
         cancelText="Cancel"
       >
         {modalContent === 'deleteSection' ? (
--- Frontend/src/features/configPage/section.jsx
+++ Frontend/src/features/configPage/section.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal, Input } from 'antd';
+import { Button, Modal, Input, Radio } from 'antd';
 import { SettingOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const Section = ({ title, content1, content2, onDelete, onEdit }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showContentOption, setShowContentOption] = useState('hide');
   const [newTitle, setNewTitle] = useState(title);
   const [newContent1, setNewContent1] = useState(content1);
   const [newContent2, setNewContent2] = useState(content2);
@@ -22,11 +23,19 @@ const Section = ({ title, content1, content2, onDelete, onEdit }) => {
     setIsModalVisible(false);
   };
 
+  const handleOptionChange = e => {
+    setShowContentOption(e.target.value);
+  };
+
   return (
-    <section className="bg-gray-100 p-4 mb-5 pb-4 relative top-16">
-      <h2 className="text-xl font-semibold mb-4">{title}</h2>
+    <section className="bg-gray-100 p-2 mb-5 pb-4 relative top-16">
+      <div style={{ padding: '0px 30%', borderRadius: '10px' }}>
+        <h2 className="text-xl font-semibold mb-4 text-center bg-white">
+          {title}
+        </h2>
+      </div>
       <p>{content1}</p>
-      <p>{content2}</p>
+      {showContentOption === 'show' && <p>{content2}</p>}{' '}
       <Button
         type="text"
         icon={<SettingOutlined />}
@@ -59,12 +68,22 @@ const Section = ({ title, content1, content2, onDelete, onEdit }) => {
           onChange={e => setNewContent1(e.target.value)}
           style={{ marginBottom: '10px' }}
         />
-        <Input
-          placeholder="Content 2"
-          value={newContent2}
-          onChange={e => setNewContent2(e.target.value)}
+        {showContentOption === 'show' && (
+          <Input
+            placeholder="Content 2"
+            value={newContent2}
+            onChange={e => setNewContent2(e.target.value)}
+            style={{ marginBottom: '10px' }}
+          />
+        )}
+        <Radio.Group
+          onChange={handleOptionChange}
+          value={showContentOption}
           style={{ marginBottom: '10px' }}
-        />
+        >
+          <Radio value="hide">Section 1</Radio>
+          <Radio value="show">Section 2</Radio>
+        </Radio.Group>
       </Modal>
     </section>
   );
